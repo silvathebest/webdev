489f0ea4c1412db9af707e2f815ecbcac9f1aaf8
silvathebest <silverchat228@gmail.com>
2019-11-18 22:01:20 +0400

    Revert "1.0"
    This reverts commit ba0ee95fbee5b307e29ba0e208aed10b3bc2bd90.

--- script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunc() {
-	var x = document.getElementById("myTopnav");
-
-	if(x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
